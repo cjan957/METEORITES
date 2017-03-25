9f9d7aa8edf21fb4aa559c8a1650b8f85713f48a
Keison <m.keison.tang@gmail.com>
2017-03-25 19:06:35 +1300

    Test

--- src/com/ttcj/components/Bat.java
+++ src/com/ttcj/components/Bat.java
@@ -3,22 +3,13 @@ package com.ttcj.components;
 public class Bat extends ObjectInfo{
 	
 	private boolean isAIBat ;
-	private int batWidth;
-	private int batLength;
 		
 	private boolean isAIBat (){
 		return this.isAIBat;
 	}
 	
 	private void setAIBat(boolean AIBat ){
-		this.isAIBat = AIBat; 
-	}
-	
-	private void setBatWidth(int batWidth){
-		this.batWidth = batWidth;
-	}
-	private void setBatLength(int batLength){
-		this.batLength = batLength;
+		this.isAIBat= AIBat; 
 	}
 	
 }
