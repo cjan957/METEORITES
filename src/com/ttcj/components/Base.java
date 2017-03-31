2627e3b08d9800a8c6dcbf54ae499339b2d86512
Keison <m.keison.tang@gmail.com>
2017-03-31 16:25:14 +1300

    Updated 'base'/'castle' class

--- src/com/ttcj/components/Base.java
+++ src/com/ttcj/components/Base.java
@@ -5,8 +5,16 @@ public class Base extends ObjectInfo{
 	private boolean isDead;		//true for dead, false for alive
 	private boolean isWinner;	//true for winner, false for loser/ undetermined
 	
+	public void setIsDead(boolean Dead){
+		this.isDead = Dead;
+	}
+	
 	public boolean isDead(){
 		return this.isDead;
+	}
+	
+	public void setIsWinner(boolean Winner){
+		this.isWinner = Winner;
 	}
 	
 	public boolean isWinner(){
